99b4679fc56d95e3787fb3462d40d4ff88132b52
Richard Westmoreland <richard.westmoreland@scottscheapflights.com>
2022-08-23 09:13:49 -0400

    require cat for subcat

--- backend/app/src/api/subcategory/content-types/subcategory/lifecycles.js
+++ backend/app/src/api/subcategory/content-types/subcategory/lifecycles.js
@@ -2,8 +2,11 @@ const { ApplicationError } = require("@strapi/utils").errors;
 
 module.exports = {
   async beforeCreate(event) {
-    console.log({ event });
-    const { result } = event;
+    if (!event.params.data.resource_category) {
+      throw new ApplicationError(
+        "Please choose a resource category for this subcategory"
+      );
+    }
     const resource = await strapi.db.query("api::resource.resource").findOne({
       where: { resourceSlug: "national-resources" },
     });
