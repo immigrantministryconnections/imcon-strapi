55298abcfcb84941f75b65f061624fde7e6c5834
Richard Westmoreland <richard.westmoreland@scottscheapflights.com>
2022-08-29 13:52:42 -0400

    google analytics

--- frontend/pages/_app.jsx
+++ frontend/pages/_app.jsx
@@ -1,5 +1,6 @@
 import { createContext } from 'react';
 
+import { useRouter } from 'next/router';
 import { SessionProvider, useSession } from 'next-auth/react';
 
 import App from 'next/app';
@@ -9,12 +10,15 @@ import { DefaultSeo } from 'next-seo';
 import { getStrapiMedia } from 'utils/media';
 import { getGlobalData } from 'utils/api';
 
+import * as gtag from '../lib/gtag';
+
 import '@/styles/globals.css';
 import { Modal } from 'utils/context/modal-context';
 
 export const GlobalContext = createContext({});
 
 const MyApp = ({ Component, pageProps: { session, ...pageProps } }) => {
+  const router = useRouter();
   // Extract the data we need
   const { globalData } = pageProps;
   if (globalData == null) {
@@ -24,8 +28,39 @@ const MyApp = ({ Component, pageProps: { session, ...pageProps } }) => {
   const { defaultSeo, favicon, metaTitleSuffix } =
     globalData.global.data.attributes;
 
+  /**
+   * Google analytics things
+   */
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on('routeChangeComplete', handleRouteChange);
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+      />
+      <Script
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
       {/* Favicon */}
       <Head>
         <link
