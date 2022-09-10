42e55b2222cabaa8bd77769033690f06fa740241
Richard Westmoreland <richard.westmoreland@scottscheapflights.com>
2022-09-09 22:57:45 -0500

    feat(backend): add mezzanine page to signup

---
+++ backend/app/src/api/mezzanine-signup-page/controllers/mezzanine-signup-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ *  mezzanine-signup-page controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::mezzanine-signup-page.mezzanine-signup-page');
---
+++ backend/app/src/api/mezzanine-signup-page/routes/mezzanine-signup-page.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * mezzanine-signup-page router.
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::mezzanine-signup-page.mezzanine-signup-page');
