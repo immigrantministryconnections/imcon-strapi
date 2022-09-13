adceaa9ef6d5e05cd589f71ec61a2cf3b7517269
Richard Westmoreland <richard.westmoreland@scottscheapflights.com>
2022-09-12 22:36:29 -0500

    style: no discs on earch results:

--- frontend/components/elements/combobox.jsx
+++ frontend/components/elements/combobox.jsx
@@ -38,7 +38,7 @@ export default function ComboBox({ onChange, results }) {
         </Combobox.Button>
 
         {searchResults?.results.length > 0 && (
-          <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
+          <Combobox.Options className="absolute z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm !list-none">
             {searchResults.results.map((org) => {
               return (
                 <Combobox.Option
